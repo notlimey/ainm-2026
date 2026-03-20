164e0f75f8d316f2fee0cf6d2290f37c1bd9c44c
Martin Myhre <70258183+notlimey@users.noreply.github.com>
2026-03-20 14:39:33 +0100

    code cleanup

--- astar-island/aggregate-data/main.ts
+++ astar-island/aggregate-data/main.ts
@@ -1,12 +1,12 @@
 import { scrapeSimulation } from "./scraper.ts";
 
-export function add(a: number, b: number): number {
-	return a + b;
-}
-
 if (!import.meta.main)
 	throw new Error("This module is not meant to be imported");
 
-await scrapeSimulation(
-	"https://app.ainm.no/submit/astar-island/replay?round=71451d74-be9f-471f-aacd-a41f3b68a9cd",
-);
+const url = Deno.args[0];
+if (!url) {
+	console.log("Usage: deno run main.ts <replay_url>");
+	Deno.exit(1);
+}
+
+await scrapeSimulation(url);
